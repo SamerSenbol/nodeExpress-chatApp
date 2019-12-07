1d13471c2941079c353de1c7b968dcbd05e6f325
Samer Senbol <samer.senbol@gmail.com>
2019-12-07 22:48:06 +0100

    Initialize a new instance of socket.io by passing the http. Then I listen on the connection event for incoming sockets.

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('public'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-  });
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
