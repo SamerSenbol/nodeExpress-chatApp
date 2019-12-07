db435123f8354180f792d206618ccc5b371243f1
Samer Senbol <samer.senbol@gmail.com>
2019-12-07 19:00:04 +0100

    creating express app which starts a server and listens on port 3000 for connections. Adding the messages dialog and chat input form. Adding the chat app css design.

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
