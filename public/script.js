4e15fbd4153a195d0585d2dcb3a4fc85c42c1148
Samer Senbol <samer.senbol@gmail.com>
2019-12-24 15:32:35 +0100

    Adding if statement in the new message socket to check if the message that we have in the input field content /happy then we will run the giphy api and show the happy man.

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
+
+
 var api = "http://api.giphy.com/v1/gifs/search?";
 var apikey = "&api_key=dc6zaTOxFJmzC";
 var query = "&q=happy";
 const url ="http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&q=happy";
-
 /* function gotData(giphy){
 
   fetch(url).then((resp) => {
@@ -55,13 +56,22 @@ $(function (){
   });
 
   socket.on('new message', function(data){
-    $chat.append('<div class="well"><strong>'+data.user+'</strong>:'+data.msg+'</div>');
+
+    if(data.msg==='/happy'){
+    $chat.append('<div class="well"><strong>'+data.user+'</strong>:<img src="https://media.giphy.com/media/l4pTfx2qLszoacZRS/giphy.gif" width=200 height=200/>'+'</div>'); 
+    }
+    else{
+      $chat.append('<div class="well"><strong>'+data.user+'</strong>:'+data.msg+'</div>'); 
+
+    }
   });
 
   $userFormArea.submit(function(e){
     e.preventDefault();
     socket.emit('new user', $username.val(), function(data){
+      
       if(data){
+        
         $userFormArea.hide();
         $messageArea.show();
       }
